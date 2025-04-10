bac438324eab92736126534c57349eb10b91e874
Koichi_Suehiro <k024g0001@g.neec.ac.jp>
2025-04-11 00:07:06 +0900

    00_05の実装

--- Math.cpp
+++ Math.cpp
@@ -427,4 +427,27 @@ Matrix4x4 MakeRotateXYZMatrix(const float& thetaX, const float& thetaY, const fl
 	return matrix;
 }
 
+//4x4アフィン変換行列を高速に生成
+Matrix4x4 MakeAffineMatrix(const Vector3<float>& scale, const Vector3<float>& rotate, const Vector3<float>& translate) {
+	Matrix4x4 rotateMatrix = MakeRotateXYZMatrix(rotate.x, rotate.y, rotate.z);
+	static Matrix4x4 affineMatrix;
+	affineMatrix.m[0][0] = scale.x * rotateMatrix.m[0][0];
+	affineMatrix.m[0][1] = scale.x * rotateMatrix.m[0][1];
+	affineMatrix.m[0][2] = scale.x * rotateMatrix.m[0][2];
+	affineMatrix.m[0][3] = 0.0f;
+	affineMatrix.m[1][0] = scale.y * rotateMatrix.m[1][0];
+	affineMatrix.m[1][1] = scale.y * rotateMatrix.m[1][1];
+	affineMatrix.m[1][2] = scale.y * rotateMatrix.m[1][2];
+	affineMatrix.m[1][3] = 0.0f;
+	affineMatrix.m[2][0] = scale.z * rotateMatrix.m[2][0];
+	affineMatrix.m[2][1] = scale.z * rotateMatrix.m[2][1];
+	affineMatrix.m[2][2] = scale.z * rotateMatrix.m[2][2];
+	affineMatrix.m[2][3] = 0.0f;
+	affineMatrix.m[3][0] = translate.x;
+	affineMatrix.m[3][1] = translate.y;
+	affineMatrix.m[3][2] = translate.z;
+	affineMatrix.m[3][3] = 1.0f;
+	return affineMatrix;
+}
+
 #pragma endregion
